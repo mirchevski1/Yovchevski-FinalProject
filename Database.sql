2d328aa799d1247797fdfa18952ed7ae8653c297
user <user@DESKTOP-U43FOB2>
2023-04-02 12:26:14 +0200

    Add project files.

---
+++ Database.sql
@@ -0,0 +1,2 @@
+﻿ALTER TABLE [Table]
+RENAME TO [Car];
---
+++ Table3.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+	[id] INT NOT NULL PRIMARY KEY, 
+    [first_name] NCHAR(10) NOT NULL, 
+    [last_name] NCHAR(10) NOT NULL, 
+    [gender] NCHAR(10) NULL, 
+    [email] NCHAR(10) NOT NULL
+)
